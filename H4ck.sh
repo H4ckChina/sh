c8e0b0e23f16e10083a52e42ce49dca70dc169d3
H4ck <103381997+H4ckChina@users.noreply.github.com>
2025-05-06 12:10:59 +0800

    Update H4ck.sh

--- H4ck.sh
+++ H4ck.sh
@@ -189,13 +189,13 @@ function main_menu() {
   while true; do
     clear
     printf "+------------------------------------------------------+\n"
-    printf "| %-20s | %-20s |\n" "H4ck China Sh" "v$sh_v"
+    printf "|        H4ck China Sh       |          v$sh_v         |\n"
     printf "+------------------------------------------------------+\n"
-    printf "| %-20s | %-20s |\n"      "1. 系统命令"  "2. Proxy"
+    printf "|         1. 系统命令        |          2. Proxy       |\n"
     printf "+------------------------------------------------------+\n"
-    printf "| %-20s | %-20s |\n"      "3. Get"      "4. Scan"
+    printf "|         3. Get            |          4. Scan         |\n"
     printf "+------------------------------------------------------+\n"
-    printf "| %-20s |\n"              "0. 退出"
+    printf "|                        0. 退出                       |\n"
     printf "+------------------------------------------------------+\n"
     echo 
     read -p "请选择菜单选项: " menuChoice
