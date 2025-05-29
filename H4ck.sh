63aff1c6a8789f96643239e2246ed4317ef8652d
H4ck <103381997+H4ckChina@users.noreply.github.com>
2025-05-29 22:11:49 +0800

    Update H4ck.sh

--- H4ck.sh
+++ H4ck.sh
@@ -26,6 +26,9 @@ function system_commands_menu() {
     1)
       echo "正在更新系统..."
       # 示例：以Ubuntu为例，更新系统
+      sudo cp -a /etc/apt/sources.list /etc/apt/sources.list.bak
+      sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.aliyun.com/@g" /etc/apt/sources.list
+      sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.aliyun.com/@g" /etc/apt/sources.list
       # 清理缓存并更新
       apt clean
       rm -rf /var/lib/apt/lists/*
